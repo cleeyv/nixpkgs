7ccc6bc46dcc9a853b64ef9fd24e9f22a80c62f8
Yaya <github@uwu.is>
2021-09-07 18:23:25 +0200

    jigasi: init at 1.1-231-gd792d0f
    Jitsi Gateway to SIP: a server-side application that allows regular SIP
    clients to join Jitsi Meet conferences hosted by Jitsi Videobridge.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22611,6 +22611,8 @@ with pkgs;
 
   jicofo = callPackage ../servers/jicofo { };
 
+  jigasi = callPackage ../servers/jigasi { };
+
   jitsi-meet = callPackage ../servers/web-apps/jitsi-meet { };
 
   jitsi-meet-prosody = callPackage ../misc/jitsi-meet-prosody { };
